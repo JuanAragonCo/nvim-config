f3208ea54e7baee3e37c8d318c4617ea01a4f36d
Juan Aragon <me@juanaragon.co>
2024-01-31 20:10:48 -0500

    (feat): Open quicklist on telescope
    After going to definition using the LSP, use the quickfix from
    telescope to list the results if multiple

--- init.lua
+++ init.lua
@@ -385,7 +385,14 @@ local on_attach = function(_, bufnr)
 	nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 	nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-	nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+	nmap('gd', function ()
+		vim.lsp.buf.definition {
+			on_list = function (options)
+				vim.fn.setqflist({}, ' ', options)
+				require('telescope.builtin').quickfix()
+			end
+		}
+	end, '[G]oto [D]efinition')
 	nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 	nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
 	nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
