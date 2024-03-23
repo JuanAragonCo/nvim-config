20e15b1f71bc23e12a224ab159310a4e4248cda2
Juan Aragon <juanaragon@Juans-MacBook-Pro.local>
2024-03-23 12:17:16 -0500

    (feat): Update tag verions

--- init.lua
+++ init.lua
@@ -45,12 +45,12 @@ require('packer').startup(function(use)
 		requires = {
 			'nvim-tree/nvim-web-devicons', -- optional, for file icons
 		},
-		tag = 'nightly' -- optional, updated every week. (see issue #1193)
+		tag = 'v1.1' -- optional, updated every week. (see issue #1193)
 	}
 
 	use {
 		'j-hui/fidget.nvim',
-		tag = 'legacy',
+		tag = 'v1.4.0',
 		config = function()
 			require("fidget").setup {
 				-- options
